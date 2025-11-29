7a27de19bf40502af499f3a998bb75c1ff9fe619
YuvarajAjay-Lab <yuvarajk.official1@gmail.com>
2025-11-29 20:02:56 +0530

    Delete Assignment1.java

--- Assignment1.java
+++
@@ -1,9 +0,0 @@
-// Write a program to check if given number is odd or even
-
-public class Assignment1 {
-    public static void main(String[] args) {
-        int a=15;
-        String number[] = {"Even","Odd"};
-        System.out.println("The given number is " + number[a%2]);
-    }
-}
