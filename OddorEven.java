0ee4c54b1e294881eca9e294d3b0a19c135ef86d
Yuvaraj_Ajay <yuvarajk.official1@gmail.com>
2025-11-29 19:58:14 +0530

    Odd or Even Program

---
+++ OddorEven.java
@@ -0,0 +1,9 @@
+// Write a program to check if given number is odd or even
+
+public class OddorEven {
+    public static void main(String[] args) {
+        int a=15;
+        String number[] = {"Even","Odd"};
+        System.out.println("The given number is " + number[a%2]);
+    }
+}
